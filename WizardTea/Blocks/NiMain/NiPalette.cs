e839b8ebca5c2031cd822a7926acd5e9b2f2b1ca
glomdom <22115@uktc-bg.com>
2024-12-10 22:09:10 +0200

    feat: parse `NiPalette`

---
+++ WizardTea/Blocks/NiMain/NiPalette.cs
@@ -0,0 +1,20 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class NiPalette : NiObject {
+    public byte HasAlpha { get; set; }
+    public uint NumEntries { get; set; }
+    public ByteColor4[] Palette { get; set; }
+
+    public NiPalette(NifStream stream) {
+        HasAlpha = stream.ReadByte();
+        NumEntries = stream.ReadUInt32();
+
+        var entries = NumEntries == 16 ? 16 : 256;
+        Palette = new ByteColor4[entries];
+        for (var i = 0; i < entries; i++) {
+            Palette[i] = new ByteColor4(stream);
+        }
+    }
+}
