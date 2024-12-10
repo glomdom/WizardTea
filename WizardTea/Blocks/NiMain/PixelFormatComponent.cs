deafa6613530537c339d9108e99be604d0160aba
glomdom <22115@uktc-bg.com>
2024-12-10 22:19:59 +0200

    feat: parse `PixelFormatComponent`

---
+++ WizardTea/Blocks/NiMain/PixelFormatComponent.cs
@@ -0,0 +1,17 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class PixelFormatComponent {
+    public PixelComponent Type { get; set; }
+    public PixelRepresentation Convention { get; set; }
+    public byte BitsPerChannel { get; set; }
+    public bool IsSigned { get; set; }
+    
+    public PixelFormatComponent(NifStream stream) {
+        Type = (PixelComponent)stream.ReadUInt32();
+        Convention = (PixelRepresentation)stream.ReadUInt32();
+        BitsPerChannel = stream.ReadByte();
+        IsSigned = stream.ReadBoolean();
+    }
+}
