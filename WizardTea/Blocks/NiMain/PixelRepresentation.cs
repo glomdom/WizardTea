dc445f3b56e0bf38b92889633ceb31f2efd61808
glomdom <22115@uktc-bg.com>
2024-12-10 22:19:49 +0200

    feat: enum `PixelRepresentation`

---
+++ WizardTea/Blocks/NiMain/PixelRepresentation.cs
@@ -0,0 +1,11 @@
+﻿namespace WizardTea.Blocks.NiMain;
+
+public enum PixelRepresentation : uint {
+    REP_NORM_INT,
+    REP_HALF,
+    REP_FLOAT,
+    REP_INDEX,
+    REP_COMPRESSED,
+    REP_UNKNOWN,
+    REP_INT
+}
