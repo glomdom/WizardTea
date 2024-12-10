3e22b06a4dbfd937f5d4e75ef2ea0e25ffbf542b
glomdom <22115@uktc-bg.com>
2024-12-10 22:18:41 +0200

    feat: enum `PixelComponent`

---
+++ WizardTea/Blocks/NiMain/PixelComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace WizardTea.Blocks.NiMain;
+
+public enum PixelComponent : uint {
+    COMP_RED,
+    COMP_GREEN,
+    COMP_BLUE,
+    COMP_ALPHA,
+    COMP_COMPRESSED,
+    COMP_OFFSET_U,
+    COMP_OFFSET_V,
+    COMP_OFFSET_W,
+    COMP_OFFSET_Q,
+    COMP_LUMA,
+    COMP_HEIGHT,
+    COMP_VECTOR_X,
+    COMP_VECTOR_Y,
+    COMP_VECTOR_Z,
+    COMP_PADDING,
+    COMP_INTENSITY,
+    COMP_INDEX,
+    COMP_DEPTH,
+    COMP_STENCIL,
+    COMP_EMPTY
+}
