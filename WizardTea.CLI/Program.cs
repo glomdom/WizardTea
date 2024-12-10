0a9bebad03c04cca238a6ea8190c2cb049590c43
glomdom <22115@uktc-bg.com>
2024-12-10 15:32:30 +0200

    feat: improve cli output

--- WizardTea.CLI/Program.cs
+++ WizardTea.CLI/Program.cs
@@ -6,18 +6,34 @@ using var stream = new NifStream(file);
 var header = new NifHeader(stream);
 
 Console.WriteLine(header.HeaderString);
-Console.WriteLine("0x" + header.Version.ToString("X"));
-Console.WriteLine(header.Endian);
-Console.WriteLine(header.UserVersion);
+Console.WriteLine($"Endian -> {header.Endian}");
+Console.WriteLine($"User Version -> {header.UserVersion}");
+Console.WriteLine($"Block Count -> {header.NumBlocks}");
+Console.WriteLine($"String Count -> {header.NumStrings}");
+Console.WriteLine($"Group Count -> {header.NumGroups}");
 
+Console.WriteLine("BlockTypes [");
 foreach (var blockType in header.BlockTypes) {
-    Console.WriteLine(blockType);
+    Console.WriteLine("  " + blockType);
 }
+Console.WriteLine("]");
 
+// X: Resolve block type for each index, would help with debugging. (?)
+Console.WriteLine("BlockTypeIndex [");
 foreach (var index in header.BlockTypeIndex) {
-    Console.WriteLine(index);
+    Console.WriteLine("  " + index);
 }
+Console.WriteLine("]");
 
-foreach (var group in header.Groups) {
-    Console.WriteLine(group);
+Console.WriteLine("Strings [");
+foreach (var str in header.Strings) {
+    Console.WriteLine("  " + str);
 }
+Console.WriteLine("]");
+
+Console.WriteLine("Groups [");
+foreach (var group in header.Groups) {
+    Console.WriteLine("  " + group);
+}
+Console.WriteLine("]");
+
