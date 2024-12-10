ab746215db722ed2d07b2df24b44de1aca0e7e9d
glomdom <22115@uktc-bg.com>
2024-12-10 20:35:15 +0200

    feat: `FilePath`

---
+++ WizardTea/Blocks/NiMain/FilePath.cs
@@ -0,0 +1,11 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class FilePath {
+    public string Index { get; set; }
+
+    public FilePath(NifStream stream, NifHeader header) {
+        Index = stream.ReadIndexString(header);
+    }
+}
