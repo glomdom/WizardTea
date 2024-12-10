1e823d12f30583029faeb2232760aebab615eb34
glomdom <22115@uktc-bg.com>
2024-12-10 22:06:37 +0200

    feat: parse `ByteColor4`

---
+++ WizardTea/Blocks/NiMain/ByteColor4.cs
@@ -0,0 +1,17 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class ByteColor4 {
+    public byte R { get; set; }
+    public byte G { get; set; }
+    public byte B { get; set; }
+    public byte A { get; set; }
+
+    public ByteColor4(NifStream stream) {
+        R = stream.ReadByte();
+        G = stream.ReadByte();
+        B = stream.ReadByte();
+        A = stream.ReadByte();
+    }
+}
