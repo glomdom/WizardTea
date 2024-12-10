c7757448d4acb0d7a9f90efec7bbb7f3aae49c20
glomdom <22115@uktc-bg.com>
2024-12-10 22:16:20 +0200

    feat: enum `PixelFormat`

---
+++ WizardTea/Blocks/NiMain/PixelFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace WizardTea.Blocks.NiMain;
+
+public enum PixelFormat : uint {
+    FMT_RGB,
+    FMT_RGBA,
+    FMT_PAL,
+    FMT_PALA,
+    FMT_DXT1,
+    FMT_DXT3,
+    FMT_DXT5,
+    FMT_RGB24NONINT,
+    FMT_BUMP,
+    FMT_BUMPLUMA,
+    FMT_RENDERSPEC,
+    FMT_1CH,
+    FMT_2CH,
+    FMT_3CH,
+    FMT_4CH,
+    FMT_DEPTH_STENCIL,
+    FMT_UNKNOWN
+}
