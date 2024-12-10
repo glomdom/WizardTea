f09b84a0959b04066c2b450c19a8db4c8079430b
glomdom <22115@uktc-bg.com>
2024-12-10 19:43:59 +0200

    feat: tribasedgeom && nitristrips

---
+++ WizardTea/Blocks/NiMain/NiTriBasedGeom.cs
@@ -0,0 +1,7 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public abstract class NiTriBasedGeom : NiGeometry {
+    protected NiTriBasedGeom(NifStream stream, NifHeader header) : base(stream, header) { }
+}
---
+++ WizardTea/Blocks/NiMain/NiTriStrips.cs
@@ -0,0 +1,7 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class NiTriStrips : NiTriBasedGeom {
+    public NiTriStrips(NifStream stream, NifHeader header) : base(stream, header) { }
+}
