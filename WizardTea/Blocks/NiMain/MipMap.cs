70be8ebdc4deca222e6b1663b857efa70f15ebb8
glomdom <22115@uktc-bg.com>
2024-12-10 22:23:34 +0200

    feat: parse `MipMap`

---
+++ WizardTea/Blocks/NiMain/MipMap.cs
@@ -0,0 +1,15 @@
+﻿using WizardTea.Core;
+
+namespace WizardTea.Blocks.NiMain;
+
+public class MipMap {
+    public uint Width { get; set; }
+    public uint Height { get; set; }
+    public uint Offset { get; set; }
+    
+    public MipMap(NifStream stream) {
+        Width = stream.ReadUInt32();
+        Height = stream.ReadUInt32();
+        Offset = stream.ReadUInt32();
+    }
+}
