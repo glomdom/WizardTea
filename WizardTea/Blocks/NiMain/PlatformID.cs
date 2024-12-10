df37dd6027b724bb4f80de87a92add415f59250d
glomdom <22115@uktc-bg.com>
2024-12-10 22:39:24 +0200

    feat: parse `PlatformID`

---
+++ WizardTea/Blocks/NiMain/PlatformID.cs
@@ -0,0 +1,13 @@
+﻿namespace WizardTea.Blocks.NiMain;
+
+public enum PlatformID : uint {
+    PLATFORM_ANY,
+    PLATFORM_XENON,
+    PLATFORM_PS3,
+    PLATFORM_DX9,
+    PLATFORM_WII,
+    PLATFORM_D3D10,
+    PLATFORM_UNKNOWN_6,
+    PLATFORM_UNKNOWN_7,
+    PLATFORM_UNKNOWN_8
+}
