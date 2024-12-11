19741a953b38f772da40c8321fb06f1ee46acf5a
glomdom <22115@uktc-bg.com>
2024-12-11 11:36:22 +0200

    ref: add doc comment

--- WizardTea.CLI/Program.cs
+++ WizardTea.CLI/Program.cs
@@ -4,8 +4,10 @@ using WizardTea.Core;
 using var file = File.OpenRead(args[0]);
 using var stream = new NifStream(file);
 
-var config = new MembersConfig() {
-    MemberFilter = provider => provider.Name != "PixelData" && provider.Name != "Vertices" && provider.Name != "UVSets" && provider.Name != "Normals"
+var config = new MembersConfig {
+    // keep this config if you don't want to spam your console with huge arrays of numbers
+    MemberFilter = provider => provider.Name != "PixelData" && provider.Name != "Vertices" &&
+                               provider.Name != "UVSets" && provider.Name != "Normals"
 };
 
 var nifFile = new NifFile(stream);
