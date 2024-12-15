d75ffe267f4af02776d7dfccb1843fcae4bf176a
glomdom <22115@uktc-bg.com>
2024-12-15 21:31:31 +0200

    feat(cli): don't display points in dump

--- WizardTea.CLI/Program.cs
+++ WizardTea.CLI/Program.cs
@@ -7,7 +7,8 @@ using var stream = new NifStream(file);
 var config = new MembersConfig {
     // keep this config if you don't want to spam your console with huge arrays of numbers
     MemberFilter = provider => provider.Name != "PixelData" && provider.Name != "Vertices" &&
-                               provider.Name != "UVSets" && provider.Name != "Normals"
+                               provider.Name != "UVSets" && provider.Name != "Normals" &&
+                               provider.Name != "Points"
 };
 
 var nifFile = new NifFile(stream);
