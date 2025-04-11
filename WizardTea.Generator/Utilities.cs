8d27d89f5633748dc4e7b893905562b65e94875c
glomdom <22115@uktc-bg.com>
2025-04-11 23:36:42 +0300

    feat: add utilities

---
+++ WizardTea.Generator/Utilities.cs
@@ -0,0 +1,14 @@
+﻿namespace WizardTea.Generator;
+
+internal static class Utilities {
+    internal static string IndentLines(string input, int spaceCount) {
+        var indent = new string(' ', spaceCount);
+
+        return string.Join(
+            Environment.NewLine,
+            input
+                .Split(["\r\n", "\n"], StringSplitOptions.None)
+                .Select(line => indent + line)
+        );
+    }
+}
