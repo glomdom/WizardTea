7f0e204c7198fd7c897d568cff89388c4e11e425
glomdom <22115@uktc-bg.com>
2025-04-12 14:00:57 +0300

    feat: add basic `Ptr` and `Ref` functionality

--- WizardTea/Overrides/Ptr.cs
+++ WizardTea/Overrides/Ptr.cs
@@ -1,5 +1,14 @@
 ﻿namespace WizardTea;
 
 public class Ptr<T> {
-    
+    private int Pointer { get; set; }
+
+    private bool HasPointer => Pointer != -1;
+
+    // TODO: Provide `NifFile` as parameter, to access blocks.
+    public T? Get() {
+        if (!HasPointer) return default;
+
+        return default;
+    }
 }
--- WizardTea/Overrides/Ref.cs
+++ WizardTea/Overrides/Ref.cs
@@ -1,5 +1,14 @@
 ﻿namespace WizardTea;
 
 public class Ref<T> {
-    
+    private int Reference { get; set; }
+
+    private bool HasReference => Reference != -1;
+
+    // TODO: Provide `NifFile` as parameter, to access blocks.
+    public T? Get() {
+        if (!HasReference) return default;
+
+        return default;
+    }
 }
